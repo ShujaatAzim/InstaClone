95d0298413b862b7f97ab009884098f7ea1e33f7
Shujaat Azim <shujaat.azim@gmail.com>
2021-03-03 21:07:08 -0500

    Initialized firebase.

--- App.js
+++ App.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import * as firebase from 'firebase';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import LandingScreen from './Components/Auth/Landing';
 
 const Stack = createStackNavigator();
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAJIJaVRRgpZesv69yhieym6bGGLX-dWpI",
+  authDomain: "instaclone-436f3.firebaseapp.com",
+  projectId: "instaclone-436f3",
+  storageBucket: "instaclone-436f3.appspot.com",
+  messagingSenderId: "446027581759",
+  appId: "1:446027581759:web:0acad14064edf1c814ab8a",
+  measurementId: "G-KHHFBF999X"
+};
+
+if (firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig)
+}
 
 const App = () => {
   return (
