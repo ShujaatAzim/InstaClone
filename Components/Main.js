08485f61bd62936af365322f2919b6c4780683ca
Shujaat Azim <shujaat.azim@gmail.com>
2021-03-07 23:59:32 -0500

    Built out Main.

---
+++ Components/Main.js
@@ -0,0 +1,17 @@
+import React, { useEffect } from 'react';
+import { View } from 'react-native';
+
+const Main = () => {
+
+  useEffect(() => {
+
+  }, [])
+  
+  return (
+    <View>
+
+    </View>
+  );
+}
+
+export default Main;
